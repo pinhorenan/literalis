09432b777da9a1a0dd63afcd5f0e3750014926cb
Renan Pinho <pinhorenan@icloud.com>
2025-06-24 21:11:44 -0300

    defeat: recomeço?

--- src/includes/comment.include.ts
+++
@@ -1,6 +0,0 @@
-import { publicUserSelect } from '@includes/user.include';
-
-export const commentInclude = {
-  author: { select: publicUserSelect },
-  likes:  { select: { user: { select: publicUserSelect } } },
-} as const;
--- src/includes/notification.include.ts
+++
@@ -1,5 +0,0 @@
-import { publicUserSelect } from '@includes/user.include';
-
-export const notificationInclude = {
-  actor: { select: publicUserSelect },
-} as const;
