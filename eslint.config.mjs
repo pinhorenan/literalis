f7d780421e8ac86f634db2056e440f1c15671a05
Renan Pinho <pinhorenan@icloud.com>
2025-06-27 17:42:50 -0300

    chores: ajustes nos arquivos iniciais de configuração do projeto.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,12 +5,16 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next", 
+    "next/core-web-vitals", 
+    "next/typescript",
+    "plugin:prettier/recommended",
+    "plugin:tailwindcss/recommended",
+  ),
 ];
 
 export default eslintConfig;
